ebab1b9cef67468ff0d7f0877f9b4cea2fbe6702
MARK777 <mark1000002@gmail.com>
2023-03-23 19:41:26 +0800

    Маленько переделал

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,12 @@ import javax.security.auth.login.LoginException;
 public class Main {
     public static void main(String[] args) {
         Authenticator user1 = new Authenticator("privet", "log3an", "logan");
-        System.out.println("checkValidityPassword(user1) = " + checkValidityPassword(user1));
+        System.out.println("checkValidityPassword(user1) = " + checkValidityPasswordForSymbol(user1));
         checkUserData(user1);
 
     }
 
-    public static boolean checkValidityLogin (Authenticator authenticator) {
+    public static boolean checkValidityLoginForSymbol (Authenticator authenticator) {
         for (int i = 0; i < authenticator.getLogin().length(); i++) {
             char symbol = authenticator.getLogin().charAt(i);
             if (!(symbol == '_'
@@ -28,7 +28,14 @@ public class Main {
         return true;
     }
 
-    public static boolean checkValidityPassword (Authenticator authenticator) {
+    public static boolean checkValidityLoginForLength (Authenticator authenticator) {
+        if (!(authenticator.getLogin().length() <= 20)) {
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean checkValidityPasswordForSymbol (Authenticator authenticator) {
         for (int i = 0; i < authenticator.getPassword().length(); i++) {
             char symbol = authenticator.getPassword().charAt(i);
             if (!(symbol == '_'
@@ -37,21 +44,38 @@ public class Main {
                     || (symbol >= '0' && symbol <= '9'))){
                 return false;
             }
-            if (!authenticator.getPassword().equals(authenticator.getConfirmPassword())) {
-                return false;
-            }
-            if (!((authenticator.getPassword().length() <= 20) || (authenticator.getConfirmPassword().length() <= 20))) {
-                return false;
-            }
+        }
+        return true;
+    }
+
+    public static boolean checkValidityPasswordForLength (Authenticator authenticator) {
+        if (!((authenticator.getPassword().length() <= 20) || (authenticator.getConfirmPassword().length() <= 20))) {
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean checkValidityPasswordForEquals (Authenticator authenticator) {
+        if (!authenticator.getPassword().equals(authenticator.getConfirmPassword())) {
+            return false;
         }
         return true;
     }
 
     public static void checkAll (Authenticator authenticator) throws WrongLoginException, WrongPasswordException {
-        if (!checkValidityLogin(authenticator)) {
+        if (!checkValidityLoginForSymbol(authenticator)) {
             throw new WrongLoginException();
         }
-        if (!checkValidityPassword(authenticator)) {
+        if (!checkValidityLoginForLength(authenticator)) {
+            throw new WrongLoginException();
+        }
+        if (!checkValidityPasswordForSymbol(authenticator)) {
+            throw new WrongPasswordException();
+        }
+        if (!checkValidityPasswordForLength(authenticator)) {
+            throw new WrongPasswordException();
+        }
+        if (!checkValidityPasswordForEquals(authenticator)) {
             throw new WrongPasswordException();
         }
     }
